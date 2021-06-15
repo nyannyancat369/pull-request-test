38dd4d7c70f05a556780aee13d7d1b3b2e22debc
nyannyancat <369.nyannyan.cat.nyaanyaa@gmail.com>
2021-06-15 14:18:13 +0900

    Create new func

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const number = [19, 3, 81, 1, 24, 21];
+
+var sortNumber = function (number) {
+    number.sort(function (a, b) {
+        if (a == b) {
+            return 0;
+        }
+        return a < b ? -1 : 1;
+    });
+};
+
+sortNumber(number);
 console.log(number);
